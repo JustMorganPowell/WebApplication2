ba52710c155fdf75824bc281ae127dce0997fdf9
Morgan Powell <powell250@hotmail.com>
2022-10-15 14:48:32 +0100

    New Updated version. Display now the .json file and fitered  on the pages.

--- WebApplication2/wwwroot/JavaScript.js
+++ WebApplication2/wwwroot/JavaScript.js
@@ -1,3 +1,30 @@
 ﻿fetch("https://localhost:7095/persons")
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(json => { 
+        const body = document.getElementById('body');
+        const type = body.dataset.type;
+        json.forEach(x => {
+            
+            
+                 
+            const div = document.createElement('div');
+            const name = document.createElement('span');
+            const position = document.createElement('span');
+            const skills = document.createElement('span');
+            name.textContent = x.name;
+            position.textContent = x.position;
+            skills.textContent = x.skills;
+            div.appendChild(name);
+            div.appendChild(position);
+            div.appendChild(skills);
+            if (type === undefined) {
+                body.appendChild(div);
+            } else if(x.position.trim().toLowerCase() === type.trim().toLowerCase()) {
+                body.appendChild(div);
+            }
+        });
+
+
+        //forEach go through the array of the .json of x is 1 person
+    });
+
